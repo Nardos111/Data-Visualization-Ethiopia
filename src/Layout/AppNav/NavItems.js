48793ed079857b4b4f7a834e3d6b93d335b46c2b
nardos111 <nardos2alemu@gmail.com>
2021-01-26 21:35:48 +0300

    Remove unused nav menus

--- src/Layout/AppNav/NavItems.js
+++ src/Layout/AppNav/NavItems.js
@@ -14,59 +14,64 @@ export const MainNav = [
         label: 'Demographic',
         to: '#/dashboards/demographic',
     },
-];
-export const EconomyNav = [
     {
         icon: 'pe-7s-angle-right',
-        label: 'GDG',
-        to: '#/tables/regular-tables',
-    },
-    {
-        icon: 'pe-7s-angle-right',
-        label: 'GDG Per Capita',
-        to: '#/elements/cards',
-    },
-    {
-        icon: 'pe-7s-angle-right',
-        label: 'Exchange Rate',
-        to: '#/elements/list-group',
+        label: 'Others',
+        to: '#/dashboards/others',
     },
 ];
-export const HealthNav = [
-    {
-        icon: 'pe-7s-angle-right',
-        label: 'Child Mortality',
-        to: '#/widgets/dashboard-boxes',
-    },
-    {
-        icon: 'pe-7s-angle-right',
-        label: 'Clean Water',
-        to: '#/components/tabs',
-    },{
-        icon: 'pe-7s-angle-right',
-        label: 'Life Expectancy',
-        to: '#/components/carousel',
-    },
-];
-export const EducationNav = [
-    {
-        icon: 'pe-7s-angle-right',
-        label: 'Gender Proportion',
-        to: '#/charts/chartjs',
-    },
-    {
-        icon: 'pe-7s-angle-right',
-        label: 'School Enrollment',
-        to: '#/dashboards/basic',
-    },
+// export const EconomyNav = [
+//     {
+//         icon: 'pe-7s-angle-right',
+//         label: 'GDG',
+//         to: '#/tables/regular-tables',
+//     },
+//     {
+//         icon: 'pe-7s-angle-right',
+//         label: 'GDG Per Capita',
+//         to: '#/elements/cards',
+//     },
+//     {
+//         icon: 'pe-7s-angle-right',
+//         label: 'Exchange Rate',
+//         to: '#/elements/list-group',
+//     },
+// ];
+// export const HealthNav = [
+//     {
+//         icon: 'pe-7s-angle-right',
+//         label: 'Child Mortality',
+//         to: '#/widgets/dashboard-boxes',
+//     },
+//     {
+//         icon: 'pe-7s-angle-right',
+//         label: 'Clean Water',
+//         to: '#/components/tabs',
+//     },{
+//         icon: 'pe-7s-angle-right',
+//         label: 'Life Expectancy',
+//         to: '#/components/carousel',
+//     },
+// ];
+// export const EducationNav = [
+//     {
+//         icon: 'pe-7s-angle-right',
+//         label: 'Gender Proportion',
+//         to: '#/charts/chartjs',
+//     },
+//     {
+//         icon: 'pe-7s-angle-right',
+//         label: 'School Enrollment',
+//         to: '#/dashboards/basic',
+//     },
     
-];
-export const PopulationNav = [
-    {
-        icon: 'pe-7s-angle-right',
-        label: 'Population',
-        to: '#/components/maps',
-    },
-];
+// ];
+// export const PopulationNav = [
+//     {
+//         icon: 'pe-7s-angle-right',
+//         label: 'Population',
+//         to: '#/components/maps',
+//     },
+// ];
 
 // to: '#/tables/regular-tables',
--- src/Layout/AppNav/VerticalNavWrapper.js
+++ src/Layout/AppNav/VerticalNavWrapper.js
@@ -14,14 +14,14 @@ class Nav extends Component {
             <Fragment>
                 <h5 className="app-sidebar__heading">Menu</h5>
                 <MetisMenu content={MainNav} activeLinkFromLocation className="vertical-nav-menu" iconNamePrefix="" classNameStateIcon="pe-7s-angle-down"/>
-                <h5 className="app-sidebar__heading">ECONOMY</h5>
+                {/* <h5 className="app-sidebar__heading">ECONOMY</h5>
                 <MetisMenu content={EconomyNav} activeLinkFromLocation className="vertical-nav-menu" iconNamePrefix="" classNameStateIcon="pe-7s-angle-down"/>
                 <h5 className="app-sidebar__heading">HEALTH</h5>
                 <MetisMenu content={HealthNav} activeLinkFromLocation className="vertical-nav-menu" iconNamePrefix="" classNameStateIcon="pe-7s-angle-down"/>
                 <h5 className="app-sidebar__heading">EDUCATION</h5>
                 <MetisMenu content={EducationNav} activeLinkFromLocation className="vertical-nav-menu" iconNamePrefix="" classNameStateIcon="pe-7s-angle-down"/>
                 <h5 className="app-sidebar__heading">POPULATION</h5>
-                <MetisMenu content={PopulationNav} activeLinkFromLocation className="vertical-nav-menu" iconNamePrefix="" classNameStateIcon="pe-7s-angle-down"/>
+                <MetisMenu content={PopulationNav} activeLinkFromLocation className="vertical-nav-menu" iconNamePrefix="" classNameStateIcon="pe-7s-angle-down"/> */}
             </Fragment>
         );
     }
